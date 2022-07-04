06ab5f1f2dbf1bb77380c546c08b11e30e36d1a4
Rahmat Rians <rahmatriansyah57@gmail.com>
2022-07-05 00:43:14 +0700

    fixing responsiveness for section one

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
 import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+
 import Card from './components/Card/Index'
 
 import Image from './assets/iconly/Image.svg'
 import Notification from './assets/iconly/Notification.svg'
 import Trolly from './assets/iconly/Trolly.svg'
+import LongArrowRight from './assets/own-icons/Long-Arrow-Right.svg'
+
+const OnMobile = ({ children }) => {
+  const mobile = useMediaQuery({ maxWidth: 640 })
+  return mobile ? children : null
+}
+const OnTablet = ({ children }) => {
+  const tablet = useMediaQuery({ minWidth: 641, maxWidth: 768 })
+  return tablet ? children : null
+}
+const OnNotebook = ({ children }) => {
+  const notebook = useMediaQuery({ minWidth: 641, maxWidth: 768 })
+  return notebook ? children : null
+}
+const OnDesktop = ({ children }) => {
+  const desktop = useMediaQuery({ minWidth: 1024 })
+  return desktop ? children : null
+}
+
 
 function App() {
   const [count, setCount] = useState(0);
   let x = [5, 5, 5];
+  let y = [2, 2, 2, 2, 2, 2, 2, 2, 2];
+
+  // const layout = layoutGenerator({
+  //   sm: 640,
+  //   md: 768,
+  //   endHalfLg: 800,
+  //   lg: 1024,
+  // });
+  // const OnMobile = layout.isAtMost('sm');
+  // const OnMostTablet = layout.isAtMost('md');
+  // const OnTablet = layout.isAtLeast('md');
+  // const OnHalfDesktop = layout.isAtLeast('endHalfLg');
+  // const OnDesktop = layout.isAtLeast('lg');
+
 
   return (
     <>
       {/* navigation */}
-      <nav className="h-[85px] flex items-center bg-base-100 shadow-xl shadow-blue-50">
+      <nav className="relative z-50 fixed top-0 left-0 right-0 h-[85px] flex items-center bg-base-100 shadow-xl shadow-blue-50">
         <div className='container mx-auto'>
           <div className='navbar'>
             <div className="navbar-start">
@@ -72,66 +107,110 @@ function App() {
 
       <section className='md:mt-20 mt-10'>
         <div className='container mx-auto'>
-          {/* banner */}
-          <div className="carousel md:rounded-[40px] rounded-lg md:h-96 h-40">
-            <div id="slide1" className="carousel-item relative w-full md:overflow-hidden">
-              <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn bg-white btn-circle text-gray-900">❮</a>
-                <a href="#slide2" className="btn bg-white btn-circle text-gray-900">❯</a>
+          <div className='mx-4 md:mx-0'>
+            {/* banner */}
+            <div className="carousel md:rounded-[40px] rounded-lg md:h-96 h-40">
+              <div id="slide1" className="carousel-item relative w-full md:overflow-hidden">
+                <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide4" className="btn bg-white btn-circle text-gray-900">❮</a>
+                  <a href="#slide2" className="btn bg-white btn-circle text-gray-900">❯</a>
+                </div>
+              </div>
+              <div id="slide2" className="carousel-item relative w-full md:overflow-hidden">
+                <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide1" className="btn bg-white btn-circle text-gray-900">❮</a>
+                  <a href="#slide3" className="btn bg-white btn-circle text-gray-900">❯</a>
+                </div>
+              </div>
+              <div id="slide3" className="carousel-item relative w-full md:overflow-hidden">
+                <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide2" className="btn bg-white btn-circle text-gray-900">❮</a>
+                  <a href="#slide4" className="btn bg-white btn-circle text-gray-900">❯</a>
+                </div>
+              </div>
+              <div id="slide4" className="carousel-item relative w-full md:overflow-hidden">
+                <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                  <a href="#slide3" className="btn bg-white btn-circle text-gray-900">❮</a>
+                  <a href="#slide1" className="btn bg-white btn-circle text-gray-900">❯</a>
+                </div>
               </div>
             </div>
-            <div id="slide2" className="carousel-item relative w-full md:overflow-hidden">
-              <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide1" className="btn bg-white btn-circle text-gray-900">❮</a>
-                <a href="#slide3" className="btn bg-white btn-circle text-gray-900">❯</a>
-              </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full md:overflow-hidden">
-              <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide2" className="btn bg-white btn-circle text-gray-900">❮</a>
-                <a href="#slide4" className="btn bg-white btn-circle text-gray-900">❯</a>
-              </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full md:overflow-hidden">
-              <img src="https://placeimg.com/800/400/arch" className="w-screen md:h-fit" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide3" className="btn bg-white btn-circle text-gray-900">❮</a>
-                <a href="#slide1" className="btn bg-white btn-circle text-gray-900">❯</a>
-              </div>
-            </div>
-          </div>
-          {/* end of banner */}
+            {/* end of banner */}
 
-          {/* category shortcut */}
-          <div className="mt-12 py-14 px-16 flex justify-center h-[85px] rounded-[40px] whitespace-nowrap md:gap-x-10 items-center shadow-xl shadow-blue-50">
-            <a href='' className='flex gap-x-4 hover:bg-hover py-1 px-6 place-content-center'><img src={Image} /><p className='font-Inter md:md-title1 title1 text-secColor'>Semua Kategori</p></a>
-            <a><p className='text-secColor'>|</p></a>
-            <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='font-Inter md:md-title1 title1 text-secColor'>Software</p></a>
-            <a><p className='text-secColor'>|</p></a>
-            <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='font-Inter md:md-title1 title1 text-secColor'>Gadget</p></a>
-            <a><p className='text-secColor'>|</p></a>
-            <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='font-Inter md:md-title1 title1 text-secColor'>Pakaian</p></a>
-            <a><p className='text-secColor'>|</p></a>
-            <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='font-Inter md:md-title1 title1 text-secColor'>Alat Tulis</p></a>
+            {/* category shortcut */}
+            <OnDesktop>
+              <div className="mt-12 py-14 px-16 flex justify-center h-[85px] rounded-[40px] whitespace-nowrap md:gap-x-10 items-center shadow-xl shadow-blue-50">
+                <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='md:md-title1 title1 text-secColor'>Semua Kategori</p></a>
+                <a><p className='text-secColor'>|</p></a>
+                <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='md:md-title1 title1 text-secColor'>Software</p></a>
+                <a><p className='text-secColor'>|</p></a>
+                <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='md:md-title1 title1 text-secColor'>Gadget</p></a>
+                <a><p className='text-secColor'>|</p></a>
+                <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='md:md-title1 title1 text-secColor'>Pakaian</p></a>
+                <a><p className='text-secColor'>|</p></a>
+                <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-6 place-content-center'><img src={Image} /><p className='md:md-title1 title1 text-secColor'>Alat Tulis</p></a>
+              </div>
+            </OnDesktop>
+            <>
+              <OnNotebook>
+                <div className="mt-12 py-14 px-16 flex justify-center h-[85px] rounded-[40px] whitespace-nowrap md:gap-x-10 items-center shadow-xl shadow-blue-50">
+                  <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Semua Kategori</p></a>
+                  <a><p className='text-secColor'>|</p></a>
+                  <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Software</p></a>
+                  <a><p className='text-secColor'>|</p></a>
+                  <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Gadget</p></a>
+                  <a><p className='text-secColor'>|</p></a>
+                  <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Pakaian</p></a>
+                  <a><p className='text-secColor'>|</p></a>
+                  <a href='' className='flex gap-x-4 hover:bg-hover hover:mb-2 py-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Alat Tulis</p></a>
+                </div>
+              </OnNotebook>
+            </>
+            <OnMobile>
+              <div className="mt-12 py-4 px-8 grid grid-cols-2 h-fit rounded-[20px] whitespace-nowrap md:gap-x-10 shadow-xl shadow-blue-50">
+                <a href='' className='col-span-2 flex gap-x-2 hover:bg-hover py-2 m-1 px-3 place-content-center'><img src={Image} /><p className='title1 text-secColor'>Semua Kategori</p></a>
+                <a href='' className='flex gap-x-2 hover:bg-hover py-2 m-1 px-3'><img src={Image} /><p className='title1 text-secColor'>Software</p></a>
+                <a href='' className='flex gap-x-2 hover:bg-hover py-2 m-1 px-3'><img src={Image} /><p className='title1 text-secColor'>Gadget</p></a>
+                <a href='' className='flex gap-x-2 hover:bg-hover py-2 m-1 px-3'><img src={Image} /><p className='title1 text-secColor'>Pakaian</p></a>
+                <a href='' className='flex gap-x-2 hover:bg-hover py-2 m-1 px-3'><img src={Image} /><p className='title1 text-secColor'>Alat Tulis</p></a>
+              </div>
+            </OnMobile>
+            {/* end of category shortcut */}
           </div>
-          {/* end of category shortcut */}
         </div>
       </section>
 
-      <section className='md:mt-20 mt-10 bg-[#f5f5f5]'>
+      {/* <section className='md:mt-20 mt-10 bg-[#f5f5f5]'>
         <div className='container grid grid-cols-5 md:py-[180px] mx-auto'>
-          <div className='col-span-2'>
-            <p className='md:md-header1 mb-2 animate-bounce'>Flash Deal</p>
-            <p className='font-reguler text-[28px] mb-2'>Berakhir Dalam</p>
-            <p className='md:md-header1 mb-2'>01 : 44 : 07</p>
-            <button className="btn pb-[70px] pt-[60px] px-[70px] rounded-[40px] bg-gradient-to-r border-none from-[#FF3A75] to-[#FF6E5A]"><p className='md-title1 font-semibold text-white capitalize'>Lihat Semua</p></button>
+          <div className='col-span-2 grid content-between'>
+            <div>
+              <p className='lg:lg-header1 md:md-header1 mb-2 animate-bounce text-secColor'>Flash Deal</p>
+              <p className='font-reguler text-[28px] mb-2 text-secColor opacity-[.55]'>Berakhir Dalam</p>
+              <p className='lg:lg-header1 md:md-header1 mb-2 text-secColor'>01 : 44 : 07</p>
+            </div>
+            <button className="btn w-fit gap-x-8 pb-[85px] pt-[60px] px-[70px] rounded-[40px] bg-gradient-to-r border-none from-[#FF3A75] to-[#FF6E5A]"><p className='md-title1 font-semibold text-white capitalize'>Lihat Semua</p><img src={LongArrowRight} width='52' /></button>
           </div>
           <div className='col-span-3'>
             <div className='flex gap-x-6'>
               {x.map((val, idx) => (
+                <Card key={idx} />
+              )
+              )}
+            </div>
+          </div>
+        </div>
+      </section> */}
+
+      <section className='md:mt-20 mt-10'>
+        <div className='container mt-40 bg-red-9900 mx-auto'>
+          <div className='mx-4 md:mx-0'>
+            <p className='lg:lg-header1 md:md-header1 sm-header1 md:mb-[115px] mb-10 text-secColor'>Rekomendasi Buat Kamu</p>
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 md:gap-x-6 gap-x-4 md:gap-y-12 gap-y-8'>
+              {y.map((val, idx) => (
                 <Card key={idx} />
               )
               )}
--- src/components/Card/Index.jsx
+++ src/components/Card/Index.jsx
@@ -5,32 +5,34 @@ import Love from '../../assets/own-icons/Love.svg'
 function Card() {
     return (
         <>
-            <div className="inline-block card p-4 pb-6 rounded-[30px] w-[300px] bg-base-100 shadow-xl shadow-blue-100">
-                <figure className="">
-                    <img src="https://placeimg.com/400/400/arch" alt="Shoes" className="rounded-2xl" />
-                    <div className="absolute flex justify-between right-8 top-8">
-                        <span className='bg-white rounded-full shadow-md  p-3'><img src={Love} width='24' /></span>
+            <a href='#' className='inline-block'>
+                <div className="card p-4 pb-6 rounded-[30px]  bg-base-100 shadow-xl shadow-blue-100">
+                    <figure className="">
+                        <img src="https://placeimg.com/400/400/arch" alt="Shoes" className="rounded-2xl" />
+                        <div className="absolute md:right-8 md:top-8 right-7 top-7">
+                            <span className='bg-white rounded-full shadow-md grid place-content-center w-7 md:w-12 h-7 md:h-12'><img src={Love} className='w-4 md:w-6' /></span>
+                        </div>
+                    </figure>
+                    <div className="mt-6 grid gap-y-2">
+                        <p className="md:md-paragraph1 sm-paragraph1 text-secColor">Blackmores Multivitamins + Minerals</p>
+                        <span>
+                            <p className="md:md-subTitle1 sm-subTitle1 text-secColor">Rp17.000</p>
+                            <span className='flex gap-x-2'>
+                                <p className="md:lg-label1 sm-label1 md:py-0 py-1 text-secColor opacity-[.55]"><s>Rp34.000</s></p>
+                                <p className="px-2 py-1 rounded-md bg-[#FFDBE2] lg-label2 text-[#F94D6B]">50%</p>
+                            </span>
+                        </span>
+                        <span className='flex md:gap-x-2 gap-x-1'>
+                            <span className='flex gap-x-1'>
+                                <img src={Star} className='w-3 lg:w-4' />
+                                <p className="lg:lg-label1 md:md-label1 sm-label1 text-secColor opacity-[.55]">4.8</p>
+                            </span>
+                            <p className="lg:lg-label1 md:md-label1 sm-label1 text-secColor opacity-[.55]">|</p>
+                            <p className="lg:lg-label1 md:md-label1 sm-label1 text-secColor opacity-[.55]">Terjual 680 kali</p>
+                        </span>
                     </div>
-                </figure>
-                <div className="mt-6 grid gap-y-2">
-                    <p className="md-paragraph1 text-secColor">Blackmores Multivitamins + Minerals</p>
-                    <span>
-                        <p className="md-subTitle1 text-secColor">Rp17.000</p>
-                        <span className='flex gap-x-2'>
-                            <p className="md-label1 text-secColor opacity-[.55]"><s>Rp34.000</s></p>
-                            <p className="px-2 py-1 rounded-md bg-[#FFDBE2] md-label2 text-[#F94D6B]">50%</p>
-                        </span>
-                    </span>
-                    <span className='flex gap-x-2'>
-                        <span className='flex gap-x-1'>
-                            <img src={Star} width='14' />
-                            <p className="md-label1 text-secColor opacity-[.55]">4.8</p>
-                        </span>
-                        <p className="md-label1 text-secColor opacity-[.55]">|</p>
-                        <p className="lg:md-label1 text-secColor md:text-[10px] text-[6px] opacity-[.55]">Terjual 680 kali</p>
-                    </span>
                 </div>
-            </div>
+            </a>
         </>
     )
 }
